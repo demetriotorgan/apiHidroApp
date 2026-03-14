afd88b61c1ff681577b773d1dbb98a9296750f16
demetriotorgan <demetriotorgan@hotmail.com>
2026-03-14 19:52:29 -0300

    versaoParaDeploy01

--- index.js
+++ index.js
@@ -8,22 +8,30 @@ const routesPluviometro = require('./routes/pluviometroRoutes');
 const routesTambor = require('./routes/tamborRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({
-    origin:'*',    
-}));
-app.use((req,res, next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    next();    
-  });
 
-  mongoose
-    .connect(process.env.DATABASE_URL)
-    .then(()=>console.log('Conectado ao MongoDB com sucesso!'))
-    .catch((err)=>console.log(err));
+app.use(cors({
+    origin: '*'
+}));
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE');
+    next();
+});
+
+mongoose
+.connect(process.env.DATABASE_URL)
+.then(()=>console.log('Conectado ao MongoDB com sucesso!'))
+.catch((err)=>console.log(err));
 
 app.use('/', routesHidrometro, routesPluviometro, routesTambor);
-app.listen(PORT, ()=>console.log(`Rodando na porta ${PORT}`));
+
+// roda localmente
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+}
+
+module.exports = app;
